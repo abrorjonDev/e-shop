31865bef7b7d689e9f54436471d1f5d960e9a81d
Abrorjon Axmadov <86520471+abrorjonDev@users.noreply.github.com>
2023-06-16 12:31:39 +0500

    Update views.py
    some optimize in views

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-
-# TO DO
-    # LOGIN
-    # LOGOUT
-    # PASSWORD RESET
-    # PASSWORD CHANGE
-    # REGISTER
+from rest_framework.viewsets import ModelViewSet
 
 from .serializers import CompanyInfoSerializer
 from .models import CompanyInfo
-from rest_framework.viewsets import ModelViewSet
+
+
 class CompanyDataAPIView(ModelViewSet):
+    """
+    All methods are built-in methods in ViewSet.
+    It's unefficient, but more faster in dev process.
+    """
     queryset = CompanyInfo.objects.all()
     serializer_class = CompanyInfoSerializer
-    
